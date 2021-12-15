c740cc8a51629debbe5972280429c6b151c7ca44
liviadelgaudio <livia.delgaudio@studio.unibo.it>
2021-12-15 17:03:46 +0100

    prodotti added in the db

--- db/popolaDb.sql
+++ db/popolaDb.sql
@@ -34,3 +34,72 @@ INSERT INTO `evento` (`idEvento`, `nomeEvento`, `descrizioneEvento`, `capienzaEv
 INSERT INTO `recensione` (`idRecensione`, `codiceCliente`, `testoRecensione`, `valutazione`) VALUES ('1', '1', 'Che dire, come essere a casa!! Un appuntamento fisso tutti gli anni, per grandi e bambini sempre una conferma.', '5');
 
 INSERT INTO `recensione` (`idRecensione`, `codiceCliente`, `testoRecensione`, `valutazione`) VALUES ('2', '1', 'Anche questo anno è stato bellissimo, un caloroso saluto alla mascotte Fluffy Candy', '5');
+
+
+--prodotti
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('1', 'T-shirt logo donna', 'da inserire', 'Maglietta da donna in 100% cotone con logo del parco.', '11.99', 'Bianco', 'XS', ‘5’);
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES (‘2’, 'T-shirt logo donna', 'da inserire', 'Maglietta da donna in 100% cotone con logo del parco.', '11.99', 'Bianco', 'S', ‘5’);
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES (‘3’, 'T-shirt logo donna', 'da inserire', 'Maglietta da donna in 100% cotone con logo del parco.', '11.99', 'Bianco', ‘M’, ‘5’);
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES (‘4’, 'T-shirt logo donna', 'da inserire', 'Maglietta da donna in 100% cotone con logo del parco.', '11.99', 'Bianco', ‘L’, ‘5’);
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES (‘5’, 'T-shirt logo donna', 'da inserire', 'Maglietta da donna in 100% cotone con logo del parco.', '11.99', ‘Rosa’, ‘XS’, ‘5’);
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES (‘6’, 'T-shirt logo donna', 'da inserire', 'Maglietta da donna in 100% cotone con logo del parco.', '11.99', ‘Rosa’, ‘S’, ‘5’);
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES (‘7’, 'T-shirt logo donna', 'da inserire', 'Maglietta da donna in 100% cotone con logo del parco.', '11.99', ‘Rosa’, ‘M’, ‘5’);
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES (‘8’, 'T-shirt logo donna', 'da inserire', 'Maglietta da donna in 100% cotone con logo del parco.', '11.99', ‘Rosa’, ‘L’, ‘5’);
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES (‘9’, 'T-shirt logo uomo', 'da inserire', 'Maglietta da uomo in 100% cotone con logo del parco.', '11.99', 'Nero', ’S’, '5');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('10’, 'T-shirt logo uomo', 'da inserire', 'Maglietta da uomo in 100% cotone con logo del parco.', '11.99', 'Nero', ‘M’, '5');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('11’, 'T-shirt logo uomo', 'da inserire', 'Maglietta da uomo in 100% cotone con logo del parco.', '11.99', 'Nero', 'L', '5');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('12', 'T-shirt logo uomo', 'da inserire', 'Maglietta da uomo in 100% cotone con logo del parco.', '11.99', 'Nero', 'XL', '5');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('13', 'T-shirt logo uomo', 'da inserire', 'Maglietta da uomo in 100% cotone con logo del parco.', '11.99', 'Grigia', 'S', '5');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('14', 'T-shirt logo uomo', 'da inserire', 'Maglietta da uomo in 100% cotone con logo del parco.', '11.99', 'Grigia', 'M', '0');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('15', 'T-shirt logo uomo', 'da inserire', 'Maglietta da uomo in 100% cotone con logo del parco.', '11.99', 'Grigia', 'L', '2');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('16', 'T-shirt logo uomo', 'da inserire', 'Maglietta da uomo in 100% cotone con logo del parco.', '11.99', 'Grigia', 'XL', '7');
+
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('17', 'T-shirt logo bambino', 'da inserire', 'Maglietta da bambino in 100% cotone con logo del parco.', '9.99', 'Azzurra', '4aa', '8');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('18', 'T-shirt logo bambino', 'da inserire', 'Maglietta da bambino in 100% cotone con logo del parco.', '9.99', 'Azzurra', '8aa', '8');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('19', 'T-shirt logo bambino', 'da inserire', 'Maglietta da bambino in 100% cotone con logo del parco.', '9.99', 'Rosa', '4aa', '8');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('20', 'T-shirt logo bambino', 'da inserire', 'Maglietta da bambino in 100% cotone con logo del parco.', '9.99', 'Rosa', '8aa', '3');
+--cappellini
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('21', 'Cappellino logo', 'da inserire', 'Cappellino con visiera e logo del parco.', '10', 'Bianco', 'TU', '20');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('22', 'Cappellino logo', 'da inserire', 'Cappellino con visiera e logo del parco.', '10', 'Nero', 'TU', '12');
+--impermeabili
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('23', 'Impermeabile', 'da inserire', 'Impermeabile con mascotte del parco.', '8', 'Trasparente', 'TU', '28');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('24', 'Impermeabile', 'da inserire', 'Impermeabile con mascotte del parco.', '8', 'Rosa', 'TU', '15');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('25', 'Impermeabile bambino', 'da inserire', 'Impermeabile da bambino con mascotte del parco.', '5', 'Azzurro', 'TU', '18');
+--borracce
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('26', 'Borraccia', 'da inserire', 'Borraccia termica con mascotte del parco.', '20', 'Azzurro', 'TU', '22');
+
+INSERT INTO `prodotto` (`idProdotto`, `nomeProdotto`, `imgProdotto`, `descrizioneProdotto`, `prezzoProdotoo`, `coloreProdotto`, `tagliaProdotto`, `disponibilitaProdotto`) VALUES ('27', 'Borraccia', 'da inserire', 'Borraccia termica con mascotte del parco.', '20', 'Rosa', 'TU', '30');
+
+
+
+
+
+
+
+
+
+
+
