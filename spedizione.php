b91b89f0edab8b1b4629709d24da06cbb1a638bf
Enrica <enrica.contrini@studio.unibo.it>
2022-01-26 17:24:42 +0100

    info e contatti

---
+++ spedizione.php
@@ -0,0 +1,11 @@
+<?php
+
+    require_once("bootstrap.php");
+
+    $templateParams["titolo"] = "SvagoCity - Spedizione";
+    $templateParams["nome"] = "spedizione-page.php";
+
+    require("template/base.php");
+
+
+?>
---
+++ template/page-contatti.php
@@ -0,0 +1,4 @@
+<div class=" text-center">             
+        <h1>Contattaci</h1>
+        <p>Per qualsiasi problema puoi contattarci al numero verde: 0543-235432</p>
+</div>
